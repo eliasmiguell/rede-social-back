9a1a4d8a38403474638b1b83f83d7525d7754a2e
Elias Perreira Miguel <eliasperreiramiguel@gmail.com>
2024-11-24 23:52:17 -0300

    Atualizando arquivo de conexão com o banco de dados

--- connect.js
+++ connect.js
@@ -7,10 +7,10 @@ dotenv.config()
 
 export const db = postgres({
   host: process.env.DB_HOST,
-  user: process.env.DB_USER,
   port: process.env.DB_PORT,
-  password: process.env.DB_PASS,
-  database: process.env.DB,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 })
 
 async function testConnection() {
